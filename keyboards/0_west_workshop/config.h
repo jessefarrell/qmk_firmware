1aba580f8c00320d9b2a5d03c83b9d04ad362d5e
Jesse Farrell <jessefarrell92@gmail.com>
2025-04-13 13:52:49 -0700

    RGB Matrix - Update 1
    -- Solid red displayed
    -- Next try to use the animations

--- keyboards/0_west_workshop/config.h
+++ keyboards/0_west_workshop/config.h
@@ -4,3 +4,5 @@
 #define ENCODER_RESOLUTION 1    // how many pulses the encoder registers between each detent
 #define ENCODER_MAP_KEY_DELAY 10
 #define ENCODER_DIRECTION_FLIP   // Uncomment to flip the direction of the encoder
+
+// WS2812 RGB LED
