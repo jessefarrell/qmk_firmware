fb347cc5933123f30aaa479e9a84bfb32ef9d579
Jesse Farrell <jessefarrell92@gmail.com>
2025-04-13 13:20:54 -0700

    Rotary Encoder - Added
    -- Connected to A/B to GP27, GP28
    -- Connected switch to GP26, and put that to KEY1 for testing
    -- Originally GP27/GP28 seemed to not work... I started to go down a rabbit hole, but later recompiled and it just worked :/

---
+++ keyboards/0_west_workshop/config.h
@@ -0,0 +1,6 @@
+// Rotational encoder
+// #define ENCODER_A_PINS { GP27 }
+// #define ENCODER_B_PINS { GP28 }
+#define ENCODER_RESOLUTION 1    // how many pulses the encoder registers between each detent
+#define ENCODER_MAP_KEY_DELAY 10
+#define ENCODER_DIRECTION_FLIP   // Uncomment to flip the direction of the encoder
--- keyboards/0_west_workshop/proto/keymaps/default/keymap.c
+++ keyboards/0_west_workshop/proto/keymaps/default/keymap.c
@@ -3,6 +3,12 @@
 
 #include QMK_KEYBOARD_H
 
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+  [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+};
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
