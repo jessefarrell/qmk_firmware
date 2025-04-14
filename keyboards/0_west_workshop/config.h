b2bbc168ae39e21ef3309d120f14ba821b7405f5
Jesse Farrell <jessefarrell92@gmail.com>
2025-04-13 21:58:11 -0700

    Basic OLED Demo
    --- Displays logo on boot
    --- Redisplays whenever user interacts with keyboard

--- keyboards/0_west_workshop/config.h
+++ keyboards/0_west_workshop/config.h
@@ -1,3 +1,19 @@
+/* Copyright 2020 xxx
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
 // Rotational encoder
 // #define ENCODER_A_PINS { GP27 }
 // #define ENCODER_B_PINS { GP28 }
@@ -5,4 +21,9 @@
 #define ENCODER_MAP_KEY_DELAY 10
 #define ENCODER_DIRECTION_FLIP   // Uncomment to flip the direction of the encoder
 
-// WS2812 RGB LED
+// OLED Setup
+#define I2C_DRIVER I2CD0    // This is our i2c isntance ()
+#define I2C1_SCL_PIN GP13
+#define I2C1_SDA_PIN GP12
+#define OLED_DISPLAY_ADDRESS 0x3C
+#define OLED_DISPLAY_128X32
