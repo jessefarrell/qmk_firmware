1c7bfd2641d9382f3e527ede6f39583d15ab86a4
Jesse Farrell <jessefarrell92@gmail.com>
2025-05-12 22:27:34 -0700

    Add final RGB LED

--- keyboards/0_west_workshop/proto/keymaps/default/keymap.c
+++ keyboards/0_west_workshop/proto/keymaps/default/keymap.c
@@ -22,8 +22,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘
      */
     [0] = LAYOUT(
-        KC_1,
-        UG_NEXT,   KC_2,   KC_3,
+        UG_TOGG,
+        KC_1,   KC_2,   KC_3,
         KC_4,   KC_5,   KC_6,
         KC_7,   KC_8,   KC_9
     )
