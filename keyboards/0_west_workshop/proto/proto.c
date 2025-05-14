ca5f6d166104f5b986979d5ade607d7ac922c92c
Jesse Farrell <jessefarrell92@gmail.com>
2025-05-13 22:26:46 -0700

    Demo Advanced Done

--- keyboards/0_west_workshop/proto/proto.c
+++ keyboards/0_west_workshop/proto/proto.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 xxx
+/* Copyright 2022 Jesse Farrell
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,20 +13,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#pragma once
-// Rotational encoder
-// #define ENCODER_A_PINS { GP27 }
-// #define ENCODER_B_PINS { GP28 }
-#define ENCODER_RESOLUTION 5    // how many pulses the encoder registers between each detent
-#define ENCODER_MAP_KEY_DELAY 1    // delay bewtween commands maybe?
-#define ENCODER_DIRECTION_FLIP   // Uncomment to flip the direction of the encoder
 
-// OLED Setup
-#define I2C_DRIVER I2CD0    // This is our i2c isntance ()
-#define I2C1_SCL_PIN GP13
-#define I2C1_SDA_PIN GP12
-#define OLED_DISPLAY_ADDRESS 0x3C
-#define OLED_DISPLAY_128X32
+#include "quantum.h"
 
-// SE Logo
-#define RGBLIGHT_LAYERS
+#ifdef RGBLIGHT_ENABLE
+
+#endif
